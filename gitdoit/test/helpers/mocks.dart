b852bbc9aec7968be826a2e2143211210250e518
BerlogaBob <berloga.bob@gmail.com>
2026-02-22 23:23:56 +0000

    работа

--- gitdoit/test/helpers/mocks.dart
+++ gitdoit/test/helpers/mocks.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 /// Mock implementation of FlutterSecureStorage for testing
 class MockSecureStorage implements FlutterSecureStorage {
@@ -80,4 +82,108 @@ class MockSecureStorage implements FlutterSecureStorage {
   }) async {
     return Map.from(_storage);
   }
+}
+
+/// Mock HTTP Client for testing
+class MockHttpClient implements http.Client {
+  int statusCode = 200;
+  dynamic responseBody;
+  Map<String, String>? responseHeaders;
+  bool shouldThrow = false;
+  String? throwMessage;
+
+  @override
+  Future<http.Response> get(Uri url, {Map<String, String>? headers}) async {
+    if (shouldThrow) {
+      throw http.ClientException(throwMessage ?? 'Network error');
+    }
+    return http.Response(
+      json.encode(responseBody ?? {}),
+      statusCode,
+      headers: responseHeaders ?? <String, String>{},
+    );
+  }
+
+  @override
+  Future<http.Response> post(
+    Uri url, {
+    Map<String, String>? headers,
+    body,
+    Encoding? encoding,
+  }) async {
+    if (shouldThrow) {
+      throw http.ClientException(throwMessage ?? 'Network error');
+    }
+    return http.Response(
+      json.encode(responseBody ?? {}),
+      statusCode,
+      headers: responseHeaders ?? <String, String>{},
+    );
+  }
+
+  @override
+  Future<http.Response> put(
+    Uri url, {
+    Map<String, String>? headers,
+    body,
+    Encoding? encoding,
+  }) async {
+    if (shouldThrow) {
+      throw http.ClientException(throwMessage ?? 'Network error');
+    }
+    return http.Response(
+      json.encode(responseBody ?? {}),
+      statusCode,
+      headers: responseHeaders ?? <String, String>{},
+    );
+  }
+
+  @override
+  Future<http.Response> patch(
+    Uri url, {
+    Map<String, String>? headers,
+    body,
+    Encoding? encoding,
+  }) async {
+    if (shouldThrow) {
+      throw http.ClientException(throwMessage ?? 'Network error');
+    }
+    return http.Response(
+      json.encode(responseBody ?? {}),
+      statusCode,
+      headers: responseHeaders ?? <String, String>{},
+    );
+  }
+
+  @override
+  Future<http.Response> delete(Uri url, {Map<String, String>? headers}) async {
+    if (shouldThrow) {
+      throw http.ClientException(throwMessage ?? 'Network error');
+    }
+    return http.Response(
+      json.encode(responseBody ?? {}),
+      statusCode,
+      headers: responseHeaders ?? <String, String>{},
+    );
+  }
+
+  @override
+  Future<http.Response> read(Uri url, {Map<String, String>? headers}) async {
+    if (shouldThrow) {
+      throw http.ClientException(throwMessage ?? 'Network error');
+    }
+    return http.Response(
+      json.encode(responseBody ?? {}),
+      statusCode,
+      headers: responseHeaders ?? <String, String>{},
+    );
+  }
+
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) async {
+    return http.StreamedResponse(Stream.empty(), 200);
+  }
+
+  @override
+  void close() {}
 }

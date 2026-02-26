c20f82d08bcc11692ab240876dfeeffafa5e9ad0
BerlogaBob <berloga.bob@gmail.com>
2026-02-26 12:04:11 +0000

    total delete. preparing for new project base

--- gitdoit/android/app/src/main/kotlin/com/example/gitdoit/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.gitdoit
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- qwen-session.md
+++
@@ -1,1 +0,0 @@
-qwen --resume b240a77d-c0f1-4936-bf00-10aebdd5f1f3
